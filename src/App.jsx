2bcccd94328bff858111830a63ecff80aed04215
Kunal-Khare <kunalkhare47@gmail.com>
2025-10-28 22:26:56 +0530

    hero-image

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,7 @@ function App() {
   return (
     <>
       <div className="layout">
-      
         <Container />
-       
       </div>
     </>
   );
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -4,10 +4,17 @@ import Hero from "./Hero";
 
 function Container() {
   return (
-    <div className="container">
-      <Navbar />
-      <Hero />
-    </div>
+    <>
+      <div className="container">
+        <Navbar />
+        <Hero />
+      </div>
+
+      <div className="hero-img-container">
+        <div className="horizontal-line"/>
+        <img className="hero-img" src="./hh.jpg" alt=""  />
+      </div>
+    </>
   );
 }
 
